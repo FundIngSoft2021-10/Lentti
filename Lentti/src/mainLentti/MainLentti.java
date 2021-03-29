a3f63fb9a257a2c88b2b7aa5f5a9265c70091a8f
MrZ0mbi3 <38089758+MrZ0mbi3@users.noreply.github.com>
2021-03-29 16:03:15 -0500

    Add files via upload

---
+++ Lentti/src/mainLentti/MainLentti.java
@@ -0,0 +1,13 @@
+
+package mainLentti;
+
+import Ventanas.InicioSesion;
+
+public class MainLentti {
+
+    public static void main(String[] args) {
+      InicioSesion iniciador= new InicioSesion();
+      iniciador.setVisible(true);
+    }
+    
+}
